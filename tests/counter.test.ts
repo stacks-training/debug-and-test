910832ec7ec546e5fa40b01477161693fd3dcbac
Jonathan Nolasco <jnolascob@gmail.com>
2024-12-01 08:55:50 -0500

    Complete unit test for counter contract

--- tests/counter.test.ts
+++ tests/counter.test.ts
@@ -4,20 +4,31 @@ import { Cl } from "@stacks/transactions";
 
 const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
+const address2 = accounts.get("wallet_2")!;
 
 /*
   The test below is an example. To learn more, read the testing documentation here:
   https://docs.hiro.so/stacks/clarinet-js-sdk
 */
 
-describe("example tests", () => {
+describe("counter tests", () => {
   it("ensures simnet is well initalised", () => {
     expect(simnet.blockHeight).toBeDefined();
   });
 
+  it("get default user counter should be 0", () => {
+    let { result } = simnet.callReadOnlyFn(
+      "counter",
+      "get-user-count",
+      [ Cl.standardPrincipal(address2) ],
+      address1,
+    );
+    expect(result).toBeUint(0);
+  });
+
   it("global counter should be start at 0", () => {
     let { result } = simnet.callReadOnlyFn(
-      "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.counter",
+      "counter",
       "get-global-count",
       [],
       address1,
@@ -27,17 +38,46 @@ describe("example tests", () => {
 
   it("global counter increment function", () => {
     let { result } = simnet.callPublicFn(
-      "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.counter",
+      "counter",
       "increment",
       [],
       address1,
     );
-    console.log('=> result:', result);
     expect(result).toBeOk(Cl.bool(true));
   });
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+  it("global counter decrement function", () => {
+    let { result } = simnet.callPublicFn(
+      "counter",
+      "decrement",
+      [],
+      address1,
+    );
+    expect(result).toBeOk(Cl.bool(true));
+  });
+
+  it("validate how increment function works", () => {
+    // call increment function
+    let { result } = simnet.callPublicFn(
+      "counter",
+      "increment",
+      [],
+      address1,
+    );
+    // check global counter value
+    let globalCounter = simnet.getDataVar(
+      "counter",
+      "global-count",
+    )
+    // check user counter value
+    let userCounter = simnet.getMapEntry(
+      "counter",
+      "counters",
+      Cl.standardPrincipal(address1),
+    );
+    // validate all results
+    expect(result).toBeOk(Cl.bool(true));
+    expect(globalCounter).toBeInt(1);
+    expect(userCounter).toBeSome(Cl.uint(1));
+  });
 });
